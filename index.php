fb8205ca28565992bbef9adfbd9f758703895355
Marc Davies <marcdaviesriot@gmail.com>
2019-08-17 19:27:58 -0700

    add index.php file with basic greeting

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    $message = 'My first PHP Project';
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>helloworld</title>
+  </head>
+  <body>
+    <h1><?php echo $message; ?></h1>
+  </body>
+</html>
